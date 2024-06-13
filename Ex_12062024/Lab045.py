145b9f70b1916167c19b5586eb1ab0a622a669c8
GauravShriwas <gauravshriwas21@gmail.com>
2024-06-14 01:37:21 +0530

    Literals and Operators

---
+++ Ex_12062024/Lab045.py
@@ -0,0 +1,13 @@
+# print(input("Please Enter your Name\n"))
+# print(int(input("Please Enter your Age\n")))
+
+name = "Gaurav"
+age = 31
+
+if name == "Gaurav":
+    if age >= 30:
+        print("Allowed")
+    else:
+        print("Not Allowed!, wrong age")
+else:
+    print("Wrong Name")
